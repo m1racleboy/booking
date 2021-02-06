93cda9c0aed8957c79ecdc64449837e5e769e8f3
Nikita <miracleboy3228@mail.ru>
2021-02-06 21:12:53 +0300

    Правит названия и генерацию массива преимуществ

--- js/main.js
+++ js/main.js
@@ -40,7 +40,7 @@ const PHOTOS = [
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
 ];
 
-const numberOfObjects = 10;
+const NUMBER_OF_OBJECTS = 10;
 
 const getRandomNumber = (min, max) => {
   min = +min;
@@ -49,68 +49,81 @@ const getRandomNumber = (min, max) => {
   if ((max >= min) && (min >= 0) && (max > 0)) {
     return Math.random() * (max - min) + min;
   }
-
   else {
     return 0;
   }
-
-}
+};
 
 const getPoint = (x, y, numberOfSings = 0) => {
   let result = getRandomNumber(x, y);
   numberOfSings = +numberOfSings;
   result = numberOfSings >= 0 ? +result.toFixed(numberOfSings) : 0;
   return result;
-}
-
-const getRandomArrayElement = (elements) => {
-  return elements[~~getRandomNumber(0, elements.length - 1)];
 };
 
-const createOffer = (x, y) => {
+const getRandomArrayElement = (elements) => {
+  return elements[~~getRandomNumber(0, elements.length)];
+};
+
+const getFeaturesArray = () => {
   let featureItems = [];
 
-  for (let i = 0; i < ~~getRandomNumber(1, 7); i++) {
+  for (let i = 0; i < Math.round(getRandomNumber(1, 7)); i++) {
     featureItems[i] = getRandomArrayElement(FEATURES);
   }
 
-  featureItems = [... new Set(featureItems)];
+  featureItems.sort();
 
+  for (let i = 0; i < featureItems.length; i++) {
+    let count = 0;
+    for (let j = i + 1; j < featureItems.length; j++) {
+      if (featureItems[i] === featureItems[j]) {
+        count++;
+      }
+    }
+    if (count > 0) {
+      featureItems.splice(i + 1, count);
+    }
+  }
+  return featureItems;
+};
+
+const createOffer = (location) => {
   return {
     title: `Заголовок - ${~~getRandomNumber(1, 10)}`,
-    address: `Координата по x: ${x} Координата по y: ${y}`,
+    address: `Координата по x: ${location.x} Координата по y: ${location.y}`,
     price: ~~getRandomNumber(2000, 10000),
     type: getRandomArrayElement(TYPES),
     rooms: ~~getRandomNumber(1, 3),
     guests: ~~getRandomNumber(4, 20),
     checkin: getRandomArrayElement(CHECKINS),
     checkout: getRandomArrayElement(CHECKOUTS),
-    features: featureItems,
+    features: getFeaturesArray(),
     description: getRandomArrayElement(DESCRIPTIONS),
     photos: getRandomArrayElement(PHOTOS),
   }
-}
+};
 
 const createLocation = () => {
   return {
     x: getPoint(35.65, 35.7, 5),
     y: getPoint(139.7, 139.8, 5),
   }
-}
+};
 
-const returnObjectAd = () => {
+const returnMockData = () => {
   let objArray = [];
-  for (let i = 0; i < numberOfObjects; i++) {
+  for (let i = 0; i < NUMBER_OF_OBJECTS; i++) {
     let location = createLocation();
     objArray[i] = {
       author: {
         avatar: `img/avatars/user0${~~getRandomNumber(0, 9)}.png`,
       },
-      offer: createOffer(location.x, location.y),
+      offer: createOffer(location),
       location: location,
     }
   }
   return objArray;
-}
+};
 
-returnObjectAd();
+returnMockData();
