7acf7a63945f07d84b1550312a0904e257f71aa6
Nikita <miracleboy3228@mail.ru>
2021-02-16 03:03:01 +0300

    Правит названия констант и перекидывает мок-данные в mock.js

--- js/constant.js
+++ js/constant.js
@@ -1,62 +1,22 @@
-
-const TYPES = [
-  'palace',
-  'flat',
-  'house',
-  'bungalow',
-];
-
-const CHECKINS = [
-  '12:00',
-  '13:00',
-  '14:00',
-];
-
-const CHECKOUTS = [
-  '12:00',
-  '13:00',
-  '14:00',
-];
-
-const FEATURES = [
-  'wifi',
-  'dishwasher',
-  'parking',
-  'washer',
-  'elevator',
-  'conditioner',
-];
-
-const DESCRIPTIONS = [
-  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, culpa. Eaque beatae dolorem quas exercitationem nemo iure quibusdam iste facilis, aperiam suscipit itaque deleniti obcaecati fugit at deserunt fuga non?',
-  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit voluptatibus odit, accusantium quisquam corrupti, nobis ex distinctio debitis repudiandae perspiciatis a nihil? Sequi delectus similique odio cum. Itaque, eligendi aliquid?',
-  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis odit, voluptatum inventore neque quibusdam blanditiis mollitia sunt doloribus, accusantium ipsum animi. Nulla optio ex sapiente ut quas deserunt, sed laborum?',
-];
-
-const PHOTOS = [
-  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
-];
-
 const MIN_POSITIVE_NUMBER = 0;
-const NUMBER_OF_OBJECTS = 10;
+const COUNT_OF_MOCKS = 10;
 
-const MIN_ELEMENT = 1;
+const MIN_ELEMENTS = 1;
 
-const MIN_ELEMENT_X = 35.65;
-const MAX_ELEMENT_X = 35.7;
-
-const MIN_ELEMENT_Y = 139.7;
-const MAX_ELEMENT_Y = 139.8;
+const MIN_LOCATION_X = 35.65;
+const MAX_LOCATION_X = 35.7;
+const MIN_LOCATION_Y = 139.7;
+const MAX_LOCATION_Y = 139.8;
 
 const MIN_PRICE = 0;
 const MAX_PRICE = 1000000;
 
 const MAX_GUESTS = 4;
 const MAX_ROOMS = 7;
-const MAX_AVATARS = 9;
-const DECIMAL_PLACES = 5;
+const MAX_COUNT_OF_AVATARS = 9;
+const MAX_COUNT_OF_DECIMAL_NUMBERS = 5;
 const MAX_FEATURES = 7;
 
-export { TYPES, CHECKINS, CHECKOUTS, FEATURES, DESCRIPTIONS, PHOTOS, MIN_ELEMENT, MIN_POSITIVE_NUMBER, NUMBER_OF_OBJECTS, MIN_ELEMENT_X, MIN_ELEMENT_Y, MAX_ELEMENT_X, MAX_ELEMENT_Y, MIN_PRICE, MAX_PRICE, MAX_GUESTS, MAX_ROOMS, MAX_AVATARS, DECIMAL_PLACES, MAX_FEATURES };
+export { MIN_ELEMENTS, MIN_POSITIVE_NUMBER, COUNT_OF_MOCKS, MIN_LOCATION_X, MIN_LOCATION_Y,
+  MAX_LOCATION_X, MAX_LOCATION_Y, MIN_PRICE, MAX_PRICE, MAX_GUESTS, MAX_ROOMS,
+  MAX_COUNT_OF_AVATARS, MAX_COUNT_OF_DECIMAL_NUMBERS, MAX_FEATURES };
--- js/mock.js
+++ js/mock.js
@@ -1,5 +1,48 @@
 import { getRoundNumber, getPoint } from './util.js';
-import { TYPES, CHECKINS, CHECKOUTS, FEATURES, DESCRIPTIONS, PHOTOS, MIN_ELEMENT, MIN_POSITIVE_NUMBER, NUMBER_OF_OBJECTS, MIN_ELEMENT_X, MIN_ELEMENT_Y, MAX_ELEMENT_X, MAX_ELEMENT_Y, MIN_PRICE, MAX_PRICE, MAX_GUESTS, MAX_ROOMS, MAX_AVATARS, DECIMAL_PLACES, MAX_FEATURES } from './constant.js'
+import { MIN_ELEMENTS, MIN_POSITIVE_NUMBER, COUNT_OF_MOCKS, MIN_LOCATION_X, MIN_LOCATION_Y,
+  MAX_LOCATION_X, MAX_LOCATION_Y, MIN_PRICE, MAX_PRICE, MAX_GUESTS, MAX_ROOMS,
+  MAX_COUNT_OF_AVATARS, MAX_COUNT_OF_DECIMAL_NUMBERS, MAX_FEATURES } from './constant.js'
+
+
+const TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+];
+
+const CHECKINS = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const CHECKOUTS = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+
+const DESCRIPTIONS = [
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, culpa. Eaque beatae dolorem quas exercitationem nemo iure quibusdam iste facilis, aperiam suscipit itaque deleniti obcaecati fugit at deserunt fuga non?',
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit voluptatibus odit, accusantium quisquam corrupti, nobis ex distinctio debitis repudiandae perspiciatis a nihil? Sequi delectus similique odio cum. Itaque, eligendi aliquid?',
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis odit, voluptatum inventore neque quibusdam blanditiis mollitia sunt doloribus, accusantium ipsum animi. Nulla optio ex sapiente ut quas deserunt, sed laborum?',
+];
+
+const PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+];
 
 const getRandomArrayElement = (elements) => {
   return elements[getRoundNumber(MIN_POSITIVE_NUMBER, elements.length)];
@@ -8,22 +51,22 @@ const getRandomArrayElement = (elements) => {
 const getRandomArray = (array, length) => {
   const arrayCopy = [...array];
   for (let i = MIN_POSITIVE_NUMBER; i < (array.length - length); i++) {
-    arrayCopy.splice(getRoundNumber(MIN_POSITIVE_NUMBER, arrayCopy.length), MIN_ELEMENT)
+    arrayCopy.splice(getRoundNumber(MIN_POSITIVE_NUMBER, arrayCopy.length), MIN_ELEMENTS)
   }
   return arrayCopy;
 }
 
 const getOffer = (location) => {
   return {
-    title: `Заголовок - ${getRoundNumber(MIN_ELEMENT, NUMBER_OF_OBJECTS)}`,
+    title: `Заголовок - ${getRoundNumber(MIN_ELEMENTS, COUNT_OF_MOCKS)}`,
     address: `Координата по x: ${location.x} Координата по y: ${location.y}`,
     price: getRoundNumber(MIN_PRICE, MAX_PRICE),
     type: getRandomArrayElement(TYPES),
-    rooms: getRoundNumber(MIN_ELEMENT, MAX_ROOMS),
-    guests: getRoundNumber(MIN_ELEMENT, MAX_GUESTS),
+    rooms: getRoundNumber(MIN_ELEMENTS, MAX_ROOMS),
+    guests: getRoundNumber(MIN_ELEMENTS, MAX_GUESTS),
     checkin: getRandomArrayElement(CHECKINS),
     checkout: getRandomArrayElement(CHECKOUTS),
-    features: getRandomArray(FEATURES, getRoundNumber(MIN_ELEMENT, MAX_FEATURES)),
+    features: getRandomArray(FEATURES, getRoundNumber(MIN_ELEMENTS, MAX_FEATURES)),
     description: getRandomArrayElement(DESCRIPTIONS),
     photos: getRandomArrayElement(PHOTOS),
   }
@@ -31,18 +74,18 @@ const getOffer = (location) => {
 
 const getLocation = () => {
   return {
-    x: getPoint(MIN_ELEMENT_X, MAX_ELEMENT_X, DECIMAL_PLACES),
-    y: getPoint(MIN_ELEMENT_Y, MAX_ELEMENT_Y, DECIMAL_PLACES),
+    x: getPoint(MIN_LOCATION_X, MAX_LOCATION_X, MAX_COUNT_OF_DECIMAL_NUMBERS),
+    y: getPoint(MIN_LOCATION_Y, MAX_LOCATION_Y, MAX_COUNT_OF_DECIMAL_NUMBERS),
   }
 };
 
 const getMockData = () => {
   let objArray = [];
-  for (let i = MIN_POSITIVE_NUMBER; i < NUMBER_OF_OBJECTS; i++) {
+  for (let i = MIN_POSITIVE_NUMBER; i < COUNT_OF_MOCKS; i++) {
     let location = getLocation();
     objArray[i] = {
       author: {
-        avatar: `img/avatars/user0${getRoundNumber(MIN_ELEMENT, MAX_AVATARS)}.png`,
+        avatar: `img/avatars/user0${getRoundNumber(MIN_ELEMENTS, MAX_COUNT_OF_AVATARS)}.png`,
       },
       offer: getOffer(location),
       location: location,
