854e9a5be8216921376c0b5820f191c3032ba3b8
Nikita <miracleboy3228@mail.ru>
2021-03-13 13:42:05 +0300

    Добавляет обработчики focus/blur и рефакторит функцию дизейбла опшинов

--- js/form.js
+++ js/form.js
@@ -7,10 +7,12 @@ const checkin = document.querySelector('#timein');
 const checkout = document.querySelector('#timeout');
 const titleInput = form.querySelector('#title');
 const addressInput = form.querySelector('#address');
+const rooms = form.querySelector('#room_number');
+const capacity = form.querySelector('#capacity');
 const MIN_TITLE_LENGTH = 30;
 const MAX_TITLE_LENGTH = 100;
-const roomsCount = form.querySelector('#room_number');
-const capacitySelect = form.querySelector('#capacity');
+const MAX_ROOMS = 100;
+const NO_ROOMS = 0;
 
 const typeFieldHandler = (targetValue) => {
   const price = MIN_PRICE[targetValue];
@@ -23,29 +25,30 @@ const timeFieldHandler = (targetValue) => {
   checkin.value = targetValue;
 }
 
-const capacityFieldHandler = (targetValue) => {
-  const MAX_ROOMS = 100;
-  const NO_ROOMS = 0;
-  const rooms = [...roomsCount.children].map(element => +element.value);
-  const capacity = [...capacitySelect];
-  const capacityValues = capacity.map(element => +element.value);
-  if (targetValue === MAX_ROOMS) {
-    capacitySelect.value = NO_ROOMS;
+const optionsHandler = (options) => {
+  let memoOptions = [];
+
+  return (targetValue) => {
+    memoOptions.forEach(item => {
+      item.disabled = false;
+    });
+
+    const index = options.findIndex(elem => elem.value === targetValue);
+    const arrayToDisabled = index !== -1 ? options.slice(index + 1) : options.slice(0, options.length - 1);
+    arrayToDisabled.forEach(item => {
+      item.disabled = true;
+    });
+
+    memoOptions = [...arrayToDisabled];
   }
-  else {
-    capacitySelect.value = targetValue;
-  }
-  capacityValues.forEach((element, i) => {
-    if (element === rooms[i] && element <= targetValue && targetValue !== MAX_ROOMS) {
-      capacity[i].disabled = false;
-    }
-    else if (targetValue === MAX_ROOMS && element === NO_ROOMS) {
-      capacity[capacity.length - 1].disabled = false;
-    }
-    else {
-      capacity[i].disabled = true;
-    }
-  });
+}
+
+const capacityHandler = optionsHandler([...capacity]);
+capacityHandler(rooms.value);
+
+const capacitySelectOptionsHandler = (targetValue) => {
+  capacity.value = +targetValue === MAX_ROOMS ? NO_ROOMS : targetValue;
+  capacityHandler(targetValue);
 }
 
 const changeHandler = (e) => {
@@ -53,14 +56,19 @@ const changeHandler = (e) => {
   const targetValue = targetInput.value;
 
   switch (targetInput) {
-    case typeField: typeFieldHandler(targetValue);
+    case typeField:
+      typeFieldHandler(targetValue);
       break;
     case checkin:
-    case checkout: timeFieldHandler(targetValue);
+      timeFieldHandler(targetValue);
       break;
-    case roomsCount: capacityFieldHandler(+targetValue);
+    case checkout:
+      timeFieldHandler(targetValue);
       break;
-    default: 'something';
+    case rooms:
+      capacitySelectOptionsHandler(targetValue);
+      break;
+    default: break;
   }
 }
 
@@ -79,18 +87,14 @@ const inputHandler = () => {
 }
 
 form.addEventListener('focus', () => {
-  form.addEventListener('change', (e) => {
-    changeHandler(e);
-  });
+  form.addEventListener('change', changeHandler);
 
-  titleInput.addEventListener('input', () => {
-    inputHandler();
-  });
+  titleInput.addEventListener('input', inputHandler);
 }, true);
 
-form.addEventListener('blur', (e) => {
-  form.removeEventListener('change', changeHandler(e), true);
-  form.removeEventListener('input', inputHandler(), true);
+form.addEventListener('blur', () => {
+  form.removeEventListener('change', changeHandler, true);
+  titleInput.removeEventListener('input', inputHandler, true);
 });
 
 
