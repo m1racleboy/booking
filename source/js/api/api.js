3fa84c0232baab043398e5f3fd392b3dcc767cd1
Nikita <miracleboy3228@mail.ru>
2021-09-08 21:00:06 +0300

    Переписывает апишку на async\await

--- source/js/api/api.js
+++ source/js/api/api.js
@@ -2,24 +2,30 @@ import { ERROR_GET_MESSAGE, ERROR_POST_MESSAGE } from '../const.js';
 import { adaptDataToClient } from '../utils/adapter.js';
 import { getSimpleStructure } from '../utils/util.js';
 
-export const getData = (onSuccess, onFail) => {
-  fetch('https://23.javascript.pages.academy/keksobooking/data')
-    .then((response) => response.ok ? response.json() : onFail(ERROR_GET_MESSAGE))
-    .then((data) => {
-      const adaptedOffers = getSimpleStructure(data.map((offer) => adaptDataToClient(offer)));
-      onSuccess(adaptedOffers);
-    })
-    .catch(() => onFail(ERROR_GET_MESSAGE));
+export const getData = async (onSuccess, onFail) => {
+  try {
+    const response = await fetch('https://23.javascript.pages.academy/keksobooking/data');
+    const data = response.ok ? await response.json() : await onFail(ERROR_GET_MESSAGE);
+    const adaptedOffers = await getSimpleStructure(data.map((offer) => adaptDataToClient(offer)));
+    onSuccess(adaptedOffers);
+  }
+  catch (_) {
+    onFail(ERROR_GET_MESSAGE);
+  }
 };
 
-export const sendData = (onSuccess, onFail, body) => {
-  fetch(
-    'https://23.javascript.pages.academy/keksobooking',
-    {
-      method: 'POST',
-      body,
-    },
-  )
-    .then((response) => response.ok ? onSuccess() : onFail(ERROR_POST_MESSAGE))
-    .catch(() => onFail(ERROR_POST_MESSAGE));
+export const sendData = async (onSuccess, onFail, body) => {
+  try {
+    const response = await fetch(
+      'https://23.javascript.pages.academy/keksobooking',
+      {
+        method: 'POST',
+        body,
+      },
+    );
+    response.ok ? await onSuccess() : await onFail(ERROR_GET_MESSAGE);
+  }
+  catch (_) {
+    onFail(ERROR_POST_MESSAGE);
+  }
 };
